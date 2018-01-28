746c3983130e5a80b935fcc7b301df4b28d78016
IAN Laptop <Iannorton01@gmail.com>
2018-01-28 13:14:26 -0500

    -Added lots of fart sounds - quiet and LOUD -Chaged the fart prefab to include audio source -Changed fart script to include fart sound functionality.

--- Assets/Scripts/Fart.cs
+++ Assets/Scripts/Fart.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 [DisallowMultipleComponent]
 public class Fart : MonoBehaviour {
-
+    [Header ("Farticle Controls")]
     [SerializeField] float fadeSpeed = 0.1f;
     [SerializeField] float growSpeed = 0.01f;
     SpriteRenderer spriteRenderer;
+
+    [Header ("Fart Sounds")] //lists to contain the sounds for both quiet and loud farts
+    [SerializeField] List<AudioClip> quietFarts;
+    [SerializeField] List<AudioClip> loudFarts;
+    [SerializeField] AudioSource fartAudioSource;
+    bool fartLoud = false;
 
 
     void Start()
     {
         spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
+        PlayFart(fartLoud);
     }
 
     void Update()
     {
         GrowObject();
         FadeSprite();
+    }
+
+    void PlayFart (bool loud) 
+    {
+        if (loud)
+        {
+            fartAudioSource.clip = loudFarts[Random.Range(0, loudFarts.Count - 1)];
+        }
+        else
+        {
+            fartAudioSource.clip = quietFarts[Random.Range(0, quietFarts.Count - 1)];
+        }
+
+        fartAudioSource.Play();
+    }
+
+    public void SetVolume(bool passedVol)
+    {
+        fartLoud = passedVol;       
     }
 
     void GrowObject ()
